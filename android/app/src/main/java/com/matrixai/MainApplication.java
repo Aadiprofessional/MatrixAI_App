7bb60650e2a7a08602842d9e3ebf2e14d02e18f4
Aadiprofessional <aadiprofessional8@gmail.com>
2025-02-26 02:04:11 +0530

    android fixed

--- android/app/src/main/java/com/matrixai/MainApplication.java
+++ android/app/src/main/java/com/matrixai/MainApplication.java
@@ -1,6 +1,19 @@
+import android.app.Application;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import java.util.List;
+import com.matrixai.BuildConfig;
+import com.facebook.react.PackageList;
 
 public class MainApplication extends Application implements ReactApplication {
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -10,9 +23,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageManager().getPackages(MainApplication.this);
+      List<ReactPackage> packages = new PackageList(this).getPackages();
       packages.add(new VectorIconsPackage());
       return packages;
     }
   };
-} 
+}
